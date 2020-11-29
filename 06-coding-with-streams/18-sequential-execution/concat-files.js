65e64a59fbbffa067c87370ffc0ecb1b26d9bb0b
ryan forte <forte.ryan@hotmail.com>
2020-11-29 08:04:00 +0800

    added comments

--- 06-coding-with-streams/18-sequential-execution/concat-files.js
+++ 06-coding-with-streams/18-sequential-execution/concat-files.js
@@ -1,23 +1,28 @@
-import { createWriteStream, createReadStream } from 'fs'
-import { Readable, Transform } from 'stream'
+import { createWriteStream, createReadStream } from "fs";
+import { Readable, Transform } from "stream";
 
-export function concatFiles (dest, files) {
-  return new Promise((resolve, reject) => {
-    const destStream = createWriteStream(dest)
-    Readable.from(files) // ①
-      .pipe(new Transform({ // ②
-        objectMode: true,
-        transform (filename, enc, done) {
-          const src = createReadStream(filename)
-          src.pipe(destStream, { end: false })
-          src.on('error', done)
-          src.on('end', done) // ③
-        }
-      }))
-      .on('error', reject)
-      .on('finish', () => { // ④
-        destStream.end()
-        resolve()
-      })
-  })
+export function concatFiles(dest, files) {
+	return new Promise((resolve, reject) => {
+		const destStream = createWriteStream(dest); // this is the destination we will be writing to
+		Readable.from(files) // ①
+			.pipe(
+				new Transform({
+					// ② handles each file in the sequence where we take the whole file and transform it
+					objectMode: true,
+					transform(filename, enc, done) {
+						console.log("filename", filename);
+						const src = createReadStream(filename); // creates a readStream for the individual file
+						src.pipe(destStream, { end: false }); // takes the chunks we read from the src and pipes them into the destream where we write the chunks to the destination file
+						src.on("error", done);
+						src.on("end", done); // ③
+					},
+				})
+			)
+			.on("error", reject)
+			.on("finish", () => {
+				// ④
+				destStream.end();
+				resolve();
+			});
+	});
 }
